c7bbd0bbcd9936ea265f50d1ae8c223c5645c438
Houug <75481666+Houug@users.noreply.github.com>
2021-09-05 19:29:07 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    static double inputValue;
+
+    public static double roundAvoid(double value, int places) {
+        double scale = Math.pow(10, places);
+        return Math.round(value * scale) / scale;
+    }
+
+    public static double input() {
+        while (true)
+        {
+            Scanner sc = new Scanner(System.in);
+            try
+            {
+                System.out.println("Введите значение в метрах (формат - #####.##), чтобы перевести его в футы:");
+                return roundAvoid(sc.nextDouble(),2);
+            } catch (InputMismatchException e)
+            {
+                sc.reset();
+                System.out.println("Я не понимаю это значит! Повторите ввод!");
+            }
+        }
+    }
+
+    public static void output_before(int meters, int centimeters) {
+        System.out.println("Вы ввели "+ meters +" метра(ов) и "+ centimeters + " сантиметра(ов)");
+    }
+
+    public static void output_after(int foot, int inches) {
+        System.out.println("При переводе получилось "+ foot +" фута(ов) и "+ inches + " дюйма(ов)");
+    }
+
+    public static void conversion(double inputValue) {
+        int meters = (int) inputValue;
+        int centimeters = (int) Math.round((inputValue - meters)*100);
+
+        int totalCentimeters = meters * 100 + centimeters;
+        double inches = totalCentimeters / 2.54;
+        int foot = (int) inches / 12;
+        inches = inches % 12;
+        output_before(meters,centimeters);
+        output_after(foot, (int) inches);
+    }
+    public static void main(String[] args) {
+        inputValue = input();
+        conversion(inputValue);
+    }
+}
